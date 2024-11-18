0636e2439ee6e7a32466d12c0613599dba66cbcb
Zenisky <129884426+Vu4eke@users.noreply.github.com>
2024-11-19 02:18:35 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -3,5 +3,5 @@ getgenv().Games = {
   [4442272183] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [2]
   [7449423635] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [3]
   ------------------------------------------------------------------------------------------------------------------>
-  [9224601490] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Fruit Battleground
+  [9224601490] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation%20Hub_fb", -- Fruit Battleground
 }
