83f14f952aee425bc3f000e57f98aeaeeb2f77bf
Zenisky <129884426+Vu4eke@users.noreply.github.com>
2024-11-19 02:08:10 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,7 +1,7 @@
 getgenv().Games = {
-  [2753915549] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit [1]
-  [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit [2]
-  [7449423635] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit [3]
+  [2753915549] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [1]
+  [4442272183] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [2]
+  [7449423635] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [3]
   ------------------------------------------------------------------------------------------------------------------>
   [9224601490] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Fruit Battleground
 }
