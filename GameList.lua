5505381519e5dd1abde83468ccb38ac25dc997ba
Zenisky <129884426+Vu4eke@users.noreply.github.com>
2024-11-27 15:49:25 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -3,8 +3,9 @@ getgenv().Games = {
   [4442272183] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [2]
   [7449423635] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_bf1.lua", -- Blox Fruit [3]
   ------------------------------------------------------------------------------------------------------------------>
-  [5130598377] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_aut.lua" -- AUT Menu/Default
-  [5130598377] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_aut.lua" -- AUT played/Default
+  [5130598377] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_aut.lua" -- AUT [1]
+  [6846458508] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_aut.lua" -- AUT [2]
+  [6890041519] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation_Hub_aut.lua" -- AUT [3]
   
   [9224601490] = "https://raw.githubusercontent.com/Vu4eke/Operation-Hub_scripts/refs/heads/main/Operation%20Hub_fb", -- Fruit Battleground
 }
